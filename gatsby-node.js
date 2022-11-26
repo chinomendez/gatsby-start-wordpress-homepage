4cb2acc1647f2327c165c977d83ff6bc4ee30fca
sbsm <24718813+chinomendez@users.noreply.github.com>
2022-11-26 01:56:23 -0300

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -546,4 +546,83 @@ exports.createPages = ({ actions }) => {
     component: require.resolve("./src/components/footer.js"),
   })
 }
+
+
+// in gatsby-node.js
+exports.createSchemaCustomization = async ({ actions }) => {
+  // ...
+  actions.createTypes(`
+    type HomepageBanner implements Node & HomepageBlock {
+      id: ID!
+      blocktype: String
+      heading: String
+      text: String
+    }
+  `)
+  // ...
+}
+// ...
+exports.onCreateNode = ({ actions, node, createNodeId, createContentDigest }) => {
+}
+  // ...
+  switch (node.internal.type) {
+    case "WpPage":
+      if (node.slug !== "homepage") return
+      const {
+        homepageHero,
+        homepageCta,
+        statList,
+        testimonialList,
+        productList,
+        logoList,
+        featureList,
+        benefitList,
+        // add the new custom field group here
+        homepageBanner,
+      } = node
+
+      const heroID = createNodeId(`${node.id} >>> HomepageHero`)
+      // create an node id for the field group
+      const bannerID = createNodeId(`${node.id} >>> HomepageBanner`)
+      // ...
+
+      // create a new node for this field group
+      actions.createNode({
+        id: bannerID,
+        internal: {
+          type: "HomepageBanner",
+          contentDigest: createContentDigest(JSON.stringify(homepageBanner)),
+        },
+        parent: node.id,
+        blocktype: "HomepageBanner",
+        heading: homepageBanner.bannerHeading,
+        text: homepageBanner.bannerText,
+      })
+      // ...
+      actions.createNode({
+        ...node,
+        id: createNodeId(`${node.id} >>> Homepage`),
+        internal: {
+          type: "Homepage",
+          contentDigest: node.internal.contentDigest,
+        },
+        parent: node.id,
+        blocktype: "Homepage",
+        image: node.featuredImageId,
+        content: [
+          heroID,
+          logosID,
+          // add your banner content in the postion you would like it to appear on the page
+          bannerID,
+          productsID,
+          featuresID,
+          benefitsID,
+          statsID,
+          testimonialsID,
+          ctaID,
+        ],
+      })
+      // ...
+  }
+}
       
